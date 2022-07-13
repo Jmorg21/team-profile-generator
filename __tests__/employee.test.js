e3371860244aac77bb284701e5bac8dd79065845
Jmorg21 <Jmorg21@gmail.com>
2022-07-12 21:23:05 -0400

    libs and successful tests

---
+++ __tests__/employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require('../lib/Employee.js');
+
+test('Does this class return an object', ()=>{
+  const emp = new Employee()
+  expect(typeof(emp)).toBe("object");
+})
+
+test('Does this set a name in the class', ()=>{
+  const emp = new Employee("james")
+  expect(emp.name).toBe("james");
+  expect(emp.getName()).toBe("james");
+})
+
+test('Does this set an ID in the class', () => {
+  const emp = new Employee("123", 2)
+  expect(emp.id).toBe(2);
+  expect(emp.getId()).toBe(2)
+})
+
+test('Does this set an email in the class', () => {
+  const emp = new Employee("123", 2, "email")
+  expect(emp.email).toBe("email");
+  expect(emp.getEmail()).toBe("email");
+})
